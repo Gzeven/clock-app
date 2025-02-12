768ed44dad82768a1cac0edb1e8e5bbfdb8df8a4
Gzeven <gerben1978@hotmail.com>
2025-02-12 20:47:48 +0100

    test2

--- src/app/api/ApiCalls.ts
+++ src/app/api/ApiCalls.ts
@@ -12,15 +12,30 @@ export const fetchRandomQuote = async () => {
 
 export const fetchLocation = async () => {
   try {
-    const token = process.env.NEXT_PUBLIC_IPINFO_TOKEN;
-    const response = await axios.get(`https://ipinfo.io?token=${token}`);
-    console.log(response.data);
-    return response.data; // Returns { quote, author }
+    const response = await axios.get("http://ipinfo.io/?callback=callback&token=a48cb5037f6e2a");
+    return response.data;
   } catch (error) {
     console.error("Error fetching random quote:", error);
-    return null; // Handle errors gracefully
+    return null; 
   }
 };
+
+// export const fetchLocation = async () => {
+//   try {
+//     const token = process.env.NEXT_PUBLIC_IPINFO_TOKEN;
+//     // const response = await axios.get(`https://ipinfo.io?token=${token}`);
+//     const response = await axios.get(`http://ipinfo.io/?callback=callback&token=a48cb5037f6e2a`);
+//     https://ipinfo.io/8.8.8.8/json?token=a48cb5037f6e2a
+//     console.log("Hello Icognito");
+//     return response.data; // Returns { quote, author }
+//   } catch (error) {
+//     console.error("Error fetching random quote:", error);
+//     return null; // Handle errors gracefully
+//   }
+// };
+
+
+
 
 
 
